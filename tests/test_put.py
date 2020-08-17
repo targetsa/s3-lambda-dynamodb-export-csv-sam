d35e8c3a57ae760f3b86fef255ee3bde507d9598
Zahir Gudiño <zgudino@conwaystore.com.pa>
2020-08-17 12:35:34 -0500

    Added unit test for new DynamoDB table schema

---
+++ tests/test_put.py
@@ -0,0 +1,32 @@
+import pytest
+
+
+@pytest.mark.parametrize('key,expected_root_directory,expected_parent_directory', [
+    ('LOK-PNA-15569/LOK-PNA-15569-1.png', "LOK-PNA-15569", ""),
+    ('LOK-PNA-15569/LOK-PNA-15569-L-ROSA/LOK-PNA-15569-L-ROSA.png', "LOK-PNA-15569", "LOK-PNA-15569-L-ROSA"),
+    ('LOK-PNA-15569/LOK-PNA-15569-L/LOK-PNA-15569-L-ROSA/LOK-PNA-15569-L-ROSA.png', "LOK-PNA-15569", "LOK-PNA-15569-L"),
+    ('LOK-PNA-15569-1.png', "", ""),
+])
+def test_key_root_directory_and_parent_directory(key, expected_root_directory, expected_parent_directory):
+    key_split = key.split('/')
+    root_directory, parent_directory = key_split[:2] if len(key_split) > 2 else [
+        key_split[0] if len(key_split) > 1 else '', '']
+
+    assert (root_directory, parent_directory) == (expected_root_directory, expected_parent_directory)
+
+
+@pytest.mark.parametrize('key,expected_root_directory,expected_parent_directory', [
+    ('LOK-PNA-15569/LOK-PNA-15569-1.png', "LOK-PNA-15569", "LOK-PNA-15569"),
+    ('LOK-PNA-15569/LOK-PNA-15569-L-ROSA/LOK-PNA-15569-L-ROSA.png', "LOK-PNA-15569", "LOK-PNA-15569-L-ROSA"),
+    ('LOK-PNA-15569-1.png', "", ""),
+    ('LOK-PNA-15569/LOK-PNA-15569-L/LOK-PNA-15569-L-ROSA/LOK-PNA-15569-L-ROSA.png',
+     'LOK-PNA-15569',
+     'LOK-PNA-15569-L-ROSA')
+])
+def test_key_root_directory_and_direct_parent_directory(key, expected_root_directory, expected_parent_directory):
+    key_split = key.split('/')
+
+    root_directory = key_split[0] if len(key_split) > 1 else ''
+    parent_directory = key_split[-2] if len(key_split) > 2 else ''
+
+    assert (root_directory, parent_directory) == (expected_root_directory, expected_parent_directory)
